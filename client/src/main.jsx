6026fb7c411cdcf89f5e326e51bcb5d855b6ea3d
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-06-18 21:29:28 +0530

    Add files via upload

---
+++ client/src/main.jsx
@@ -0,0 +1,32 @@
+import React, {createContext, useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import "./index.css";
+
+// import "./styles/header.scss";
+
+
+export const server = "http://localhost:4000/api/v1/addnews";
+
+export const Context = createContext({isAuthenticated: false});
+
+const AppWrapper = ()=>{
+  const [isAuthenticated,setIsAuthenticated] = useState(false);
+  const [loading,setLoading] = useState(false);
+  const [user,setUser] = useState({});
+  return(
+    <Context.Provider value={{
+      isAuthenticated,setIsAuthenticated,
+      loading,setLoading,
+      user,setUser
+      }}>
+      <App />
+    </Context.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>,
+)
